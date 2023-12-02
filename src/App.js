edb469b902d372d97e65d325e8b7df23e7218351
Sam <sam.soghomonyan@bk.ru>
2023-12-02 21:15:23 +0400

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+  const [data,setData] = useState({})
+  useEffect(()=>{
+
+  },[])
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
